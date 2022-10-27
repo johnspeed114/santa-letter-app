a552f5e2273065a8d0f6017502b231ef6378cce3
Glitch (alj-devops-santa-app-1) <none>
2022-10-27 16:57:04 +0900

    change logic for sendmail for trial and error

--- cronJobs/cronjob.js
+++ cronJobs/cronjob.js
@@ -4,22 +4,24 @@ import { fireMail } from '../helpers/nodemailer.js';
 
 //TODO: not sure how i can send to two function
 //solution maybe we need to promisfy the firemail function?
-//ask JOHNSON about this
 export function cronjobAction() {
   cronjob.schedule('0,15,30,45 * * * * *', async () => {
     try {
       const postVar = await handleSend();
-      console.log(postVar);
+      console.log(postVar, 'eeee');
+
+      // for (let x = 0; x < postVar.length; x++) {
+      //   const data = await fireMail(
+      //     postVar[x].username,
+      //     postVar[x].address,
+      //     postVar[x].text
+      //   );
+      //   //need to add the funciton for changing hassent to 1 in the files
+      //   console.log('dfdfdfd', data);
+      // }
       if (postVar.length !== 0) {
-        for (let x = 0; x < postVar.length; x++) {
-          const data = await fireMail(
-            postVar[x].username,
-            postVar[x].address,
-            postVar[x].text
-          );
-          //need to add the funciton for changing hassent to 1 in the files
-          console.log('dfdfdfd', data);
-        }
+        await fireMail(postVar);
+        console.log(data, 'hello???');
       }
     } catch (err) {
       console.log('Error occurred. ' + err.message);
--- helpers/nodemailer.js
+++ helpers/nodemailer.js
@@ -2,40 +2,58 @@ import nodemailer from 'nodemailer';
 
 //TODO: auth needs to remove user and pass and put them env
 
-let transporter = nodemailer.createTransport({
-  host: 'smtp.ethereal.email',
-  port: 587,
-  auth: {
-    user: 'diego.marks28@ethereal.email',
-    pass: 'YvQ1FS9g8Ut8qWE1Jz',
-  },
-  debug: true,
-  logger: true,
-});
+export async function fireMail(array) {
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'diego.marks28@ethereal.email',
+      pass: 'YvQ1FS9g8Ut8qWE1Jz',
+    },
+    debug: true,
+    logger: true,
+  });
+  let emailPromiseArray = [];
+  await array.forEach(async (ele, i) => {
+    let message = {
+      from: 'do_not_reply@northpole.com',
+      to: 'santa@northpole.com',
+      subject: 'New wish from a child has been requested!',
+      html: `<p>Dear Santa Clause,<br><br>
+            A child has sent his wish. Below are the details.<br>
+            <ul>Username: ${ele.username} </ul>
+            <ul>Address: ${ele.address}</ul>
+            <ul>Wish: ${ele.text}</ul>
+            </p>`,
+    };
 
-export async function fireMail(username, address, text) {
-  let message = {
-    from: 'do_not_reply@northpole.com',
-    to: 'santa@northpole.com',
-    subject: 'New wish from a child has been requested!',
-    html: `<p>Dear Santa Clause,<br><br>
-        A child has sent his wish. Below are the details.<br>
-        <ul>Username: ${username} </ul>
-        <ul>Address: ${address}</ul>
-        <ul>Wish: ${text}</ul>
-        </p>`,
-  };
-  console.log('school');
-  transporter.sendMail(message, (error, info) => {
-    console.log('heeelo');
-    if (error) {
-      return console.log(error);
-    } else {
-      //[FYI]If callback argument is not set then the method returns a Promise object.
-      //Nodemailer itself does not use Promises internally
-      // but it wraps the return into a Promise for convenience.
-      console.log('Message sents : %s', info.messageId);
-      console.log('Message sents : %s', nodemailer.getTestMessageUrl(info));
-    }
+    await send(transporter, message)
+      .then(() => {
+        emailPromiseArray.push({ success: true, user: ele.username });
+      })
+      .catch((error) => {
+        console.log('failed to send', error);
+      });
+    console.log(check);
+  });
+}
+
+async function send(transporter, message) {
+  return new Promise(async (resolve, reject) => {
+    console.log(message, 'fi2321');
+    await transporter.sendMail(message, (error, info) => {
+      console.log('heeelo');
+      if (error) {
+        console.log(error, 'ddd');
+        return reject(error);
+      } else {
+        //[FYI]If callback argument is not set then the method returns a Promise object.
+        //Nodemailer itself does not use Promises internally
+        // but it wraps the return into a Promise for convenience.
+        // console.log('Message sents : %s', info.messageId);
+        // console.log('Message sents : %s', nodemailer.getTestMessageUrl(info));
+        return resolve();
+      }
+    });
   });
 }
